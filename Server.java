1980647043271704a92820ec28a803f2426ebb91
Nathan Wichman <wichmann@eos19.cis.gvsu.edu>
2018-11-04 15:05:53 -0500

    First Commit

---
+++ Server.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.Vector;
+/****************************************************
+ 	Runs a basic chat room server. Allows for 
+multiple users to connect at once. Allows users to
+either send messages to all other users or to just 
+specific users. Allows administors to kick specific
+users if they enter a password.
+
+@author Nathan Wichman
+@version Fall 2018
+****************************************************/
+
+public class Server{
+	public static Vector<Client> clients = new Vector<Client>();
+
+	public static void main(String args[]){
+		/**to hold all connected clients**/
+	//	Vector<Client> clients = new Vector<Client>();
+
+		/**Socket for the Server**/
+		ServerSocket serverSocket = null;
+
+		/**Port number for socket connection**/
+		int portNumber;
+
+		//Setting port number
+		portNumber = 9876;
+		
+		//Attaching the Server Socket to the port number
+		try{
+			serverSocket = new ServerSocket(portNumber);
+		}catch(IOException e){
+			System.err.println("Error in creating Server Socket at port " + portNumber);
+			System.exit(1);
+		}
+
+		while(true){
+			//Socket for the client
+			Socket clientSocket = null;
+
+			System.out.println("Waiting for client connection...");
+
+			try{
+				//Pauses until client joins the socket
+				clientSocket = serverSocket.accept();
+			}catch(IOException e){
+				System.err.println("ERROR accepting client connection");
+				System.exit(1);
+			}
+			/*Starting a new client thread to manage this 
+			client, so we can wait for a new client in this loop */
+			new ClientThread(clientSocket).start();
+		}
+	}
+}
+
+class ClientThread extends Thread{
+	private Client client;
+	private Socket clientSocket;
+	private PrintWriter out;
+	BufferedReader in;
+
+	ClientThread(Socket clientSocket){
+		this.clientSocket = clientSocket;
+		try{
+			out = new PrintWriter(clientSocket.getOutputStream(),true);
+			in = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
+		}catch(IOException e){
+			System.err.println("Error creating new client");
+		}
+	}
+
+	@Override
+	public void run(){
+		try{
+			out.println("Enter a UserName: ");
+			String userName = in.readLine(); 
+			System.out.println(userName + " has joined");
+
+			//Creating a client object, also adds client to static clients vector in this class (in constructor)
+			Client client =	new Client(clientSocket, userName);
+
+			String receivedMessage;
+
+			//Receiving messages from the Client
+			while((receivedMessage = in.readLine()) != null){
+				System.out.println(client.getUserName() + ": " + receivedMessage);
+			//	broadcastMessage("\n" + client.getUserName() + ": " + receivedMessage);
+
+				if(receivedMessage.equals("Exit")){
+					System.out.println(client.getUserName() + " has dissconnected");
+					break;
+				}
+			}
+
+			in.close();
+			out.close();
+			Server.clients.removeElement(client);
+
+			
+		}catch(IOException e){
+			System.err.println("ERROR handling client");
+		}
+
+	}
+
+	private void broadcastMessage(String message){
+		try{
+		for(Client c : Server.clients){
+			out =  new PrintWriter(c.getSocket().getOutputStream(), true);
+			out.println(message);
+		}
+		}catch(IOException e){
+			System.err.println("Error broadcasting a message to all clients");
+		}
+	}
+}
+
+class Client{
+	private Socket socket;
+	private String userName;
+
+	Client(Socket socket, String userName){
+		this.socket = socket;
+		this.userName = userName;
+		Server.clients.addElement(this);
+	}
+
+	public Socket getSocket(){
+		return socket;
+	}
+
+	public String getUserName(){
+		return userName;
+	}
+}
---
+++ User.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class User{
+	
+	public static void main(String[] args){
+		/**Server's IP Address**/
+		String ipAddress;
+
+		/**Port number for communication**/
+		int portNumber;
+
+		/**Scanner for user input**/
+		Scanner scn = new Scanner(System.in);
+
+		/**Socket to connect to Server**/
+		Socket socket = null;
+
+		/**For sending information to Server**/
+		PrintWriter out = null;
+
+		/**For receiving information from the Server**/
+		BufferedReader in = null;
+
+		/**For receiving information from Standard Input
+		 * (Probably could just use the Scanner) **/
+		BufferedReader stdIn = null;
+
+		/**Holds the most recent user input**/
+		String userInput;
+
+		try{
+			//Hard Coded Server's Ip and Port Number
+			ipAddress = "127.0.0.1";
+			portNumber = 9876;
+			
+			//Attempting to connect to Server
+			socket = new Socket(ipAddress, portNumber);
+			out = new PrintWriter(socket.getOutputStream(),true);
+			in = new BufferedReader(new InputStreamReader(
+						socket.getInputStream()));
+			stdIn = new BufferedReader(
+					new InputStreamReader(System.in));
+
+			System.out.print(in.readLine());
+			out.println(stdIn.readLine());
+
+			new inputThread(socket).start();
+
+			System.out.print("\nMessage: ");
+			while((userInput = stdIn.readLine()) != null){
+				out.println(userInput);
+				System.out.print("Message: ");
+
+				if(userInput.equals("Exit")){
+					break;
+				}
+
+			}
+
+			System.out.println("You have disconnected");
+
+
+		}catch(IOException e){
+			System.err.println("ERROR");
+			System.exit(1);
+		}
+	}	
+}
+
+class inputThread extends Thread{
+	Socket socket;
+
+	inputThread(Socket socket){
+		this.socket = socket;
+	}
+
+	@Override
+	public void run(){
+		try{
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			String inputLine;
+
+			while((inputLine = in.readLine()) != null){
+				System.out.println(inputLine);
+			}
+
+		}catch(IOException e){
+			System.err.println("Error receiving message from server");
+			System.exit(1);
+		}	
+	}
+}
